9c10fc37784ab9d26666bddfa37f6b48e099e5d5
nicolausmaximus <aniket261234@gmail.com>
2020-05-03 20:31:43 +0530

    onclick

--- index2.js
+++ index2.js
@@ -1,32 +1,4 @@
-cat21=document.getElementById("cat21");
-cat21.addEventListener("click", function(){
-    sessionStorage["xyz"] = 1;
-});
 
-cat22=document.getElementById("cat22");
-cat22.addEventListener("click", function(){
-    sessionStorage["xyz"] = 2;
-});
-
-cat23=document.getElementById("cat23");
-cat23.addEventListener("click", function(){
-    sessionStorage["xyz"] = 3;
-});
-
-cat24=document.getElementById("cat24");
-cat24.addEventListener("click", function(){
-    sessionStorage["xyz"] = 4;
-});
-
-cat25=document.getElementById("cat25");
-cat25.addEventListener("click", function(){
-    sessionStorage["xyz"] = 5;
-});
-
-cat26=document.getElementById("cat26");
-cat26.addEventListener("click", function(){
-    sessionStorage["xyz"] = 6;
-});
 
 cat11=document.getElementById("cat11");
 cat11.addEventListener("click", function(){
@@ -58,3 +30,32 @@ cat16.addEventListener("click", function(){
     sessionStorage["xyz"] = 6;
 });
 
+cat21=document.getElementById("slick-slide00");
+cat21.addEventListener("click", function(){
+    sessionStorage["xyz"] = 1;
+});
+
+cat22=document.getElementById("slick-slide01");
+cat22.addEventListener("click", function(){
+    sessionStorage["xyz"] = 2;
+});
+
+cat23=document.getElementById("slick-slide02");
+cat23.addEventListener("click", function(){
+    sessionStorage["xyz"] = 3;
+});
+
+cat24=document.getElementById("slick-slide03");
+cat24.addEventListener("click", function(){
+    sessionStorage["xyz"] = 4;
+});
+
+cat25=document.getElementById("slick-slide04");
+cat25.addEventListener("click", function(){
+    sessionStorage["xyz"] = 5;
+});
+
+cat26=document.getElementById("slick-slide05");
+cat26.addEventListener("click", function(){
+    sessionStorage["xyz"] = 6;
+});
